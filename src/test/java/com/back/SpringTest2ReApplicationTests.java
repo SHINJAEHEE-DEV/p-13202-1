b0ae2af7d2ea104c818325d7b0be130c0a79f5b5
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-05-21 16:20:21 +0900

    init

---
+++ src/test/java/com/back/SpringTest2ReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest2ReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
